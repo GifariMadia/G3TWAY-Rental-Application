78f148a98f77e9b586174bfb869bf89d9a521bba
GifariMadia <gifariputramadia@gmail.com>
2025-06-27 17:32:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,26 @@ Route::get('/', [LandingController::class, 'index'])->name('home');
 // =======================
 // Halaman Auth (Form View)
 // =======================
-Route::view('/signin', 'signin')->name('signin'); // Form signin/login
-Route::get('/profile', [UserController::class, 'show'])->name('profile.show'); // Menampilkan profil
+Route::view('/signin', 'signin')->name('signin');
+Route::get('/profile', [UserController::class, 'show'])->name('profile.show');
 
 // =======================
 // Aksi Auth (POST)
 // =======================
-Route::post('/signin', [UserController::class, 'signin'])->name('signin.process'); // Aksi signin/login
-Route::post('/register', [UserController::class, 'store'])->name('register.store'); // Aksi registrasi
+Route::post('/signin', [UserController::class, 'signin'])->name('signin.process');
+Route::post('/register', [UserController::class, 'store'])->name('register.store');
 
 // =======================
 // Rute Profil Pengguna
 // =======================
-Route::get('/profile', [UserController::class, 'show'])->name('profile.show'); // Menampilkan profil
-Route::post('/profile', [UserController::class, 'update'])->name('profile.update'); // Update profil
+Route::get('/profile', [UserController::class, 'show'])->name('profile.show');
+Route::post('/profile', [UserController::class, 'update'])->name('profile.update');
 Route::delete('/profile/{id}', [UserController::class, 'destroy'])->name('profile.destroy');
 
 // =======================
 // Rute Mobil
 // =======================
 
-// Rute untuk melihat detail mobil
 Route::get('/cars/{car_id}', [CarController::class, 'show'])->name('cars.detail');
 
 
@@ -41,24 +40,23 @@ Route::get('/cars/{car_id}', [CarController::class, 'show'])->name('cars.detail'
 // Rute Sewa Mobil
 // =======================
 Route::view('/rentals/create', 'rentals.create')->name('rentals.create');
-Route::post('/rentals/create', [RentalController::class, 'store'])->name('cars.store'); // Menambahkan data mobil baru
-Route::get('/cars', [CarController::class, 'showAllCars'])->name('cars.index'); // Menampilkan daftar mobil dengan relasi
+Route::post('/rentals/create', [RentalController::class, 'store'])->name('cars.store');
+Route::get('/cars', [CarController::class, 'showAllCars'])->name('cars.index');
 
 // =======================
 // Rute Sidebar Menu
 // =======================
 Route::view('/reviews', 'reviews.index')->name('reviews.index');
 Route::view('/reviews/create', 'reviews.create')->name('reviews.create');
-Route::post('/review/create', [CarController::class, 'update'])->name('reviews.store'); // Update review mobil
+Route::post('/review/create', [CarController::class, 'update'])->name('reviews.store');
 
 Route::get('/rentals', [RentalController::class, 'index'])->name('rentals.index');
-Route::get('/rentals/{car_id}/edit', [RentalController::class, 'edit'])->name('rentals.edit');  // Tampilkan form edit rental
+Route::get('/rentals/{car_id}/edit', [RentalController::class, 'edit'])->name('rentals.edit');
 Route::put('/rentals/{car_id}', [RentalController::class, 'update'])->name('rentals.update');
-Route::delete('/rentals/{car_id}', [RentalController::class, 'destroy'])->name('rentals.destroy'); // Hapus rental
- // Proses update rental
+Route::delete('/rentals/{car_id}', [RentalController::class, 'destroy'])->name('rentals.destroy');
 
 // =======================
-// Tes Koneksi Database (Opsional)
+// Tes Koneksi Database
 // =======================
 Route::get('/db-test', function () {
     try {
